6b707e07a4c67e17cd9d6755bde4c325f393cea4
DevOps Tinman <devops.tinman@gmail.com>
2020-04-21 16:19:03 -0500

    Initial Commit

---
+++ Goalie/Goalie/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  Goalie
+//
+//  Created by Allbee, Eamon on 4/21/20.
+//  Copyright © 2020 Allbee, Eamon. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Goalie/Goalie/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  Goalie
+//
+//  Created by Allbee, Eamon on 4/21/20.
+//  Copyright © 2020 Allbee, Eamon. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
